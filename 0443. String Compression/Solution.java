89e681b7e246a3cf7404a5d7c284373f5ce4b8c8
Pawin Nakvisai <pawinnak2@gmail.com>
2025-02-10 13:24:32 +0700

    [ADD] 0443. String Compression

---
+++ 0443. String Compression/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int idx = 0;
+
+        for (int i = 0; i < n; i++) {
+            char ch = chars[i];
+            int count = 0;
+
+            while (i < n && ch == chars[i]) {
+                count++;
+                i++;
+            }
+
+            if (count == 1) {
+                chars[idx++] = ch;
+            } else {
+                chars[idx++] = ch;
+                for (char digit : Integer.toString(count).toCharArray()) {
+                    chars[idx++] = digit;
+                }
+            }
+
+            i--;
+        }
+
+        return idx;
+    }
+}
