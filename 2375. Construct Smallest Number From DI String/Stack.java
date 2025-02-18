06d39afd94b2b299a81d3b8af660f6a608d5f38a
Pawin Nakvisai <pawinnak2@gmail.com>
2025-02-18 16:04:40 +0700

    [ADD] 2375. Construct Smallest Number From DI String

---
+++ 2375. Construct Smallest Number From DI String/Stack.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String smallestNumber(String pattern) {
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i <= pattern.length(); i++) {
+            stack.push(i + 1);
+
+            if (i == pattern.length() || pattern.charAt(i) == 'I') {
+                while (stack.size() > 0) {
+                    result.append(stack.pop());
+                }
+            }
+        }
+
+        return result.toString();
+    }
+}
