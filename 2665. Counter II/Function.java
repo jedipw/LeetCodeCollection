21380091073d93da20a3a4e389e0c5477982aafb
Pawin Nakvisai <pawinnak2@gmail.com>
2025-02-20 21:32:21 +0700

    [ADD] 2665. Counter II

---
+++ 2665. Counter II/Function.java
@@ -0,0 +1,25 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let value = init;
+    return {
+        increment: () => {
+            return ++value;
+        },
+        decrement: () => {
+            return --value;
+        },
+        reset: () => {
+            return (value = init);
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
