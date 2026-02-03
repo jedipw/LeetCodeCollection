1446ecc1639d39c0c87c1405ebf1ddc8185ed403
Pawin Nakvisai <pawinnak2@gmail.com>
2026-02-03 21:26:01 +0700

    [ADD] 3637. Trionic Array I

---
+++ 3637. Trionic Array I/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n = nums.length;
+
+        int i = 1;
+
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int p = i - 1;
+
+        while (i < n && nums[i - 1] > nums[i]) {
+            i++;
+        }
+        int q = i - 1;
+
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int flag = i - 1;
+
+        return (p != 0) && (p != q) && (flag == n - 1 && flag != q);
+    }
+}
