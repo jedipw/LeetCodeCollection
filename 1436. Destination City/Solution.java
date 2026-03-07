8545a11ad61f2b17654d502d3add24d2c317ebb1
Pawin Nakvisai <pawinnak2@gmail.com>
2026-03-07 23:12:49 +0700

    [ADD] 1436. Destination City

---
+++ 1436. Destination City/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> set = new HashSet<>();
+
+        for (List<String> path : paths) {
+            set.add(path.get(0));
+        }
+
+        for (List<String> path : paths) {
+            String candidate = path.get(1);
+            if (!set.contains(path.get(1)))
+                return candidate;
+        }
+
+        return "";
+    }
+}
