eefbdebec68bdbbf5663ee7b6ce3fe82143f4433
Pawin Nakvisai <pawinnak2@gmail.com>
2026-04-24 23:40:52 +0700

    [ADD] 1748. Sum of Unique Elements

---
+++ 1748. Sum of Unique Elements/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int count = 0;
+
+        for (int key : map.keySet()) {
+            if (map.get(key) == 1) {
+                count += key;
+            }
+        }
+
+        return count;
+    }
+}
