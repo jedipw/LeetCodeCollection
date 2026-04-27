5d217c4a2c334ccd3d78e4cfc777cd86ad69a2e9
Pawin Nakvisai <pawinnak2@gmail.com>
2026-04-27 23:19:37 +0700

    [ADD] 3898. Find the Degree of Each Vertex

---
+++ 3898. Find the Degree of Each Vertex/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int n = matrix.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i] += matrix[j][i];
+            }
+        }
+
+        return res;
+    }
+}
