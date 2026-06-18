748c90c2d6b3820bb8545a1009e12797b79be393
Pawin Nakvisai <pawinnak2@gmail.com>
2026-06-18 22:11:19 +0700

    [ADD] 2109. Adding Spaces to a String

---
+++ 2109. Adding Spaces to a String/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addSpaces(String s, int[] spaces) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(s.substring(0, spaces[0]));
+        for (int i = 1; i < spaces.length; i++) {
+            sb.append(" ");
+            sb.append(s.substring(spaces[i - 1], spaces[i]));
+        }
+        sb.append(" ");
+        sb.append(s.substring(spaces[spaces.length - 1]));
+
+        return sb.toString();
+    }
+}
