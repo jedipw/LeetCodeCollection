8a5ea3dba49f259a028fcc6e4faae9aeb8c9885b
Pawin Nakvisai <pawinnak2@gmail.com>
2026-06-30 23:41:56 +0700

    [ADD] 1407. Top Travellers

---
+++ 1407. Top Travellers/Solution.java
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select u.name, IFNULL(SUM(distance),0) as travelled_distance
+from Users u
+left join Rides r on u.id = r.user_id
+group by u.id
+order by 2 desc, 1 asc;
