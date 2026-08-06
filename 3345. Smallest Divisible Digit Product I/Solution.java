8d542856b6b8c5204bf9251387fd4ae8e704dc9f
Pawin Nakvisai <pawinnak2@gmail.com>
2026-08-06 23:25:24 +0700

    [ADD] 3345. Smallest Divisible Digit Product I

---
+++ 3345. Smallest Divisible Digit Product I/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        while (productOfDigits(n) % t != 0) {
+            n++;
+        }
+        return n;
+    }
+
+    public int productOfDigits(int n) {
+        int product = 1;
+
+        while (n > 0) {
+            product *= n % 10;
+            n /= 10;
+        }
+
+        return product;
+    }
+}
