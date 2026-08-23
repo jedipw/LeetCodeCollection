b79cc5186b7497600c789e27cf8b0a281cb5218f
Pawin Nakvisai <pawinnak2@gmail.com>
2026-08-23 23:30:24 +0700

    [ADD] 3895. Count Digit Appearances

---
+++ 3895. Count Digit Appearances/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count = 0;
+
+        for (int num : nums) {
+            while (num > 0) {
+                if (num % 10 == digit) {
+                    count++;
+                }
+                num /= 10;
+            }
+        }
+
+        return count;
+    }
+}
